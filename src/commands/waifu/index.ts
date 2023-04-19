5facba22164e34e3ae4c5b1e911420c1c4c6b036
Gab <rodriguezgab1601@gmail.com>
2023-04-20 00:59:22 +0800

    Add embed

--- src/commands/waifu/index.ts
+++ src/commands/waifu/index.ts
@@ -1,13 +1,24 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { APIEmbed, SlashCommandBuilder } from 'discord.js';
 import { sendErrorLog } from '../../utils/helpers';
 import { AppCommand, AppCommandOptions } from '../commands';
+
+const generateWaifuEmbed = (): APIEmbed => {
+  const embed = {
+    title: 'Waifu Command',
+    color: 55296,
+    description: 'Waifu goes here',
+  };
+
+  return embed;
+};
 
 export default {
   data: new SlashCommandBuilder().setName('waifu').setDescription('Shows a random waifu image'),
   async execute({ interaction }: AppCommandOptions) {
     try {
       await interaction.deferReply();
-      await interaction.editReply({ content: 'waifu' });
+      const embed = generateWaifuEmbed();
+      await interaction.editReply({ embeds: [embed] });
     } catch (error) {
       sendErrorLog({ error, interaction });
     }
