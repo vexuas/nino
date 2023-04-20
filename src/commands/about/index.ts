c791b929729f09df32be4b70ede99417372dee51
Gab <rodriguezgab1601@gmail.com>
2023-04-21 01:33:27 +0800

    Cleanup about command

--- src/commands/about/index.ts
+++ src/commands/about/index.ts
@@ -1,4 +1,4 @@
-import { APIEmbed, Client, SlashCommandBuilder } from 'discord.js';
+import { APIEmbed, Client, hyperlink, SlashCommandBuilder } from 'discord.js';
 import { BOT_UPDATED_AT, BOT_VERSION } from '../../version';
 import { AppCommand, AppCommandOptions } from '../commands';
 import { format } from 'date-fns';
@@ -8,15 +8,15 @@ export const generateAboutEmbed = (app?: Client): APIEmbed => {
   const embed = {
     title: 'Info',
     description:
-      "Hi there! This is where you'd want to explain what your App does and any other cool stuff about it! :D",
+      "Hi there! I'm Nino and I provide an easy way to get waifu images and gifs! I don't really have any preferences so I always give random ones!\n\nMy data is retrieved from https://www.waifu.im/\n\nHappy waifus!",
     color: 55296,
     thumbnail: {
-      url: 'https://cdn.discordapp.com/attachments/1089616880576245853/1094559253395689562/mitsuha.jpg',
+      url: 'https://cdn.discordapp.com/attachments/1097563418237599800/1098660924212117585/nino-waifuim1.png',
     },
     fields: [
       {
         name: 'Creator',
-        value: '-',
+        value: 'Vexuas#8141',
         inline: true,
       },
       {
@@ -42,7 +42,7 @@ export const generateAboutEmbed = (app?: Client): APIEmbed => {
       },
       {
         name: 'Support Server',
-        value: '-',
+        value: hyperlink('Come join!', 'https://discord.gg/CJrx8TtM9V'),
         inline: true,
       },
     ],
