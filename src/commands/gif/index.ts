36be3833e5f283ea2ee217a24e69c661e73a01ee
Gab <rodriguezgab1601@gmail.com>
2023-04-21 02:13:12 +0800

    Create gif command

---
+++ src/commands/gif/index.ts
@@ -0,0 +1,53 @@
+import { APIEmbed, hyperlink, SlashCommandBuilder } from 'discord.js';
+import { isEmpty, reduce } from 'lodash';
+import { WaifuSchema } from '../../schemas/waifu';
+import { getWaifu } from '../../services/adapters';
+import { sendErrorLog } from '../../utils/helpers';
+import { AppCommand, AppCommandOptions } from '../commands';
+
+export const generateGifEmbed = (data: WaifuSchema): APIEmbed => {
+  const color = parseInt(data.dominant_color.replace('#', '0x'));
+  const tags = reduce(
+    data.tags,
+    (accumulator, value) => {
+      return `${accumulator}${isEmpty(accumulator) ? '' : ', '}${value.name}`;
+    },
+    ''
+  );
+  const orientation: 'Portrait' | 'Landscape' = data.width > data.height ? 'Landscape' : 'Portrait';
+  const embed: APIEmbed = {
+    color,
+    description: `${hyperlink('Source', data.source)} | ${hyperlink('Preview', data.preview_url)}`,
+    image: {
+      url: data.url,
+    },
+    fields: [
+      {
+        name: 'Orientation',
+        value: orientation,
+        inline: true,
+      },
+      {
+        name: 'Tags',
+        value: tags,
+        inline: true,
+      },
+    ],
+  };
+  return embed;
+};
+
+export default {
+  commandType: 'Waifu',
+  data: new SlashCommandBuilder().setName('gif').setDescription('Shows a random waifu gif'),
+  async execute({ interaction }: AppCommandOptions) {
+    try {
+      await interaction.deferReply();
+      const data = await getWaifu({ isGif: true });
+      const embed = generateGifEmbed(data);
+      await interaction.editReply({ embeds: [embed] });
+    } catch (error) {
+      sendErrorLog({ error, interaction });
+    }
+  },
+} as AppCommand;
--- src/services/adapters.ts
+++ src/services/adapters.ts
@@ -3,9 +3,13 @@ import { WaifuAPI, WaifuSchema } from '../schemas/waifu';
 
 const BASE_URL = 'https://api.waifu.im';
 
-export async function getWaifu(): Promise<WaifuSchema> {
+interface WaifuProps {
+  isGif?: boolean;
+}
+
+export async function getWaifu(props?: WaifuProps): Promise<WaifuSchema> {
   const response = (await got
-    .get(`${BASE_URL}/search?orientation=RANDOM&is_nsfw=FALSE`)
+    .get(`${BASE_URL}/search?orientation=RANDOM&is_nsfw=false&gif=${props ? props.isGif : false}`)
     .json()) as WaifuAPI;
   return response.images[0];
 }
