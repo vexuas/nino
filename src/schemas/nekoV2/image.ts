3cbb84b53fd07b03a369e325d5c8e2de1333e132
Gab <rodriguezgab1601@gmail.com>
2023-04-23 22:57:57 +0800

    Move schema to its own image file

---
+++ src/schemas/nekoV2/image.ts
@@ -0,0 +1,68 @@
+export interface NekosImageV2APIObject {
+  data: NekosImageV2APISchema;
+  included?: [];
+}
+export interface NekosImageV2APISchema {
+  id: string;
+  type: 'image';
+  attributes: NekosImageV2AttributeSchema;
+  relationships: NekosImageV2RelationshipSchema;
+  links: {
+    self: 'https://api.nekosapi.com/v2/images/fb07fa64-cc3e-4ee2-a8df-fb905d457996';
+  };
+}
+export interface NekosImageV2AttributeSchema {
+  file: string;
+  title: string;
+  colors: {
+    dominant: string | null;
+    palette: string[];
+  };
+  source: {
+    name: string | null;
+    url: string | null;
+  };
+  dimens: {
+    height: number;
+    width: number;
+    aspectRation: string | null;
+  };
+  isOriginal: boolean;
+  verificationStatus: string;
+  ageRating: string;
+  timeStamps: {
+    created: string;
+    updated: string;
+  };
+}
+export interface NekosImageV2RelationshipSchema {
+  uploader: {
+    data: { id: string; type: 'user' } | null;
+    links: { self: string; related: string };
+  };
+  artist: {
+    data: { id: string; type: 'artist' } | null;
+    links: { self: string; related: string };
+  };
+  categories: {
+    meta: {
+      count: number;
+    };
+    data: { id: string; type: 'categories' }[];
+    links: { self: string; related: string };
+  };
+  characters: {
+    meta: {
+      count: number;
+    };
+    data: { id: string; type: 'characters' }[];
+    links: { self: string; related: string };
+  };
+  likedBy: {
+    meta: {
+      count: number;
+      data: { id: string; type: 'likedBy' }[];
+      links: { self: string; related: string };
+    };
+  };
+}
