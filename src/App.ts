878bc6d88d185cb11a64882363b6ec85c851a57b
Gab <rodriguezgab1601@gmail.com>
2023-04-29 20:49:03 +0800

    Create scheduled commands handler

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,14 @@
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Channel, Client, GatewayIntentBits } from 'discord.js';
 import { BOT_TOKEN, MIXPANEL_ID, FLAGSMITH_KEY } from './config/environment';
 import { registerEventHandlers } from './events/events';
 import { sendErrorLog } from './utils/helpers';
 import Mixpanel from 'mixpanel';
 import { isEmpty } from 'lodash';
 import Flagsmith from 'flagsmith-nodejs';
+import { getNekosImageV2, getOtakuGif, getOtakuReactions, getWaifu } from './services/adapters';
+import { generateWaifuEmbed } from './commands/waifu';
+import { generateGifEmbed } from './commands/gif';
+import { generateImageEmbedV2 } from './commands/image';
 
 const app: Client = new Client({
   intents: [GatewayIntentBits.Guilds],
@@ -28,5 +32,26 @@ const initialize = async (): Promise<void> => {
     sendErrorLog({ error });
   }
 };
+
+export async function sendScheduledCommands() {
+  try {
+    const waifuData = await getWaifu();
+    const reactions = await getOtakuReactions();
+    const randomReaction = reactions[Math.floor(Math.random() * reactions.length)];
+    const gifData = await getOtakuGif(randomReaction);
+    const imageData = await getNekosImageV2();
+
+    const waifuEmbed = generateWaifuEmbed(waifuData);
+    const gifEmbed = generateGifEmbed(gifData, randomReaction);
+    const imageEmbed = generateImageEmbedV2(imageData);
+
+    const scheduleChannel: Channel | undefined = app.channels.cache.get('1101852189422518342');
+    scheduleChannel &&
+      scheduleChannel.isTextBased() &&
+      (await scheduleChannel.send({ embeds: [waifuEmbed, imageEmbed, gifEmbed] }));
+  } catch (error) {
+    sendErrorLog({ error });
+  }
+}
 
 initialize();
