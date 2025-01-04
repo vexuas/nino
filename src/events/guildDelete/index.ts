4bb139c20df97520ae5f34adc1e62964debe3a63
vexuas <rodriguezgab1601@gmail.com>
2025-01-05 00:41:21 +0800

    Replace to personal cdn

--- src/events/guildDelete/index.ts
+++ src/events/guildDelete/index.ts
@@ -15,8 +15,7 @@ export default function ({ app }: EventModule) {
         await notificationWebhook.send({
           embeds: [embed],
           username: 'Nino Server Notification',
-          avatarURL:
-            'https://cdn.discordapp.com/attachments/1097563418237599800/1098660924212117585/nino-waifuim1.png',
+          avatarURL: 'https://vexuas.b-cdn.net/nino-waifuim1.png',
         });
       }
     } catch (error) {
